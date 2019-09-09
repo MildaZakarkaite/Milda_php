74b9720ca25e5c5d44a7ad5663484058aa899273
MildaZakarkaite <54890925+MildaZakarkaite@users.noreply.github.com>
2019-09-09 11:13:07 +0300

    1 uzduotis

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+print 'labas';
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Aš valdau HTML!</title>
+</head>
+<body>
+    <h1>Milda</h1>
+    <p>Moku kurti statinį turinį.</p>
+</body>
+</html>
