77841fa054b96f5053395595bf793f17617e00cf
MildaZakarkaite <54890925+MildaZakarkaite@users.noreply.github.com>
2019-09-09 12:23:40 +0300

    Data ir laikas

--- index.php
+++ index.php
@@ -1,20 +1,10 @@
-<?php
-
-print 'labas';
-
-
-?>
-
-<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Aš valdau HTML!</title>
+    <title>Aš valdau HTML <?php print 'ir PHP':?>!</title>
 </head>
 <body>
-    <h1>Milda</h1>
-    <p>Moku kurti statinį turinį.</p>
+    <h1>Milda- HTML<?php print 'ir PHP':?>asė</h1>
+    <p>Kuriu dinaminį turinį. Atidaryta:<?php print date("Y-m-d H:i:s"):?></p>
 </body>
 </html>
