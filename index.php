b5080176670a2ae8438832b4687f0414fa7ef1b8
Administrator <Administrator@IK-PC>
2019-09-13 16:48:40 +0300

    aha

--- index.php
+++ index.php
@@ -1,10 +1,54 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Aš valdau HTML <?php print 'ir PHP':?>!</title>
-</head>
-<body>
-    <h1>Milda- HTML<?php print 'ir PHP':?>asė</h1>
-    <p>Kuriu dinaminį turinį. Atidaryta:<?php print date("Y-m-d H:i:s"):?></p>
-</body>
+<?php
+
+$randomiser = rand(0, 2);
+
+if ($randomiser === 0) {
+    $coffee = 'tea';
+} elseif ($randomiser === 1) {
+    $coffee = 'black-coffee';
+} else {
+    $coffee = 'latte';
+}
+
+?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Gersiu kava/late/tea</title>
+        <link rel="stylesheet" href="includes/normalize.css">
+        <link rel="stylesheet" href="includes/style.css">
+        <style>
+            div {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+            .black-coffee {
+                height: 100vh;
+                color: white;
+                background-color: black;
+            }
+            .tea {
+                height: 100vh;
+                color: brown;
+                background-color: greenyellow;
+            }
+            .latte {
+                height: 100vh;
+                color: white;
+                background-color: burlywood;
+            }
+            p {
+                font-size: 40px;
+            }
+        </style>
+    </head>
+    <body>  
+        <div class="<?php print $coffee; ?>">
+            <p>
+                Gersiu <?php print $coffee; ?>
+            </p>
+        </div>
+    </body>
 </html>
